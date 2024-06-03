93fce13ea57f15c221677aef9813bd0e9adb4983
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-06-04 00:01:41 +0100

    Fancy menu and user class added

--- Program.cs
+++ Program.cs
@@ -1,16 +1,89 @@
 ﻿// Var declartion
-string username;
-var date = DateTime.UtcNow;
+using System.ComponentModel.Design;
+using MathGame;
 
-Console.WriteLine("Please type your name");
-username = Console.ReadLine();
+string? username = null;
+DateTime date = DateTime.UtcNow;
 
-Menu(username, date);
+while (username == null)
+{
+    Console.WriteLine("Please type your name: ");
+    username = Console.ReadLine();
+}
 
-static void Menu(string username, DateTime date)
+Menu();
+void Menu()
+{
+    Console.WriteLine("##############################################");
+    Console.WriteLine("#           Welcome to Math Game             #");
+    Console.WriteLine("#           1. New game                      #");
+    Console.WriteLine("#           2. Load game                     #");
+    Console.WriteLine("#           3. View highscores               #");
+    Console.WriteLine("#           4. Quit                          #");
+    Console.WriteLine("##############################################");
+
+    ConsoleKeyInfo keyInfo;
+    int selectedOption = ;
+
+    do
+    {
+        keyInfo = Console.ReadKey();
+
+        switch (keyInfo.Key)
+        {
+            case ConsoleKey.UpArrow:
+                selectedOption = Math.Max(selectedOption--, 1);
+                break;
+            case ConsoleKey.DownArrow:
+                selectedOption = Math.Min(selectedOption++, 4);
+                break;
+            case ConsoleKey.Enter:
+                switch (selectedOption)
+                {
+                    case 1:
+                        CreateUser();
+                        break;
+                    case 2:
+                        //SelectUser();
+                        break;
+                    case 3:
+                        //ViewHighScores();
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+                break;
+        }
+
+        Console.Clear();
+        Console.WriteLine("##############################################");
+        Console.WriteLine("#           Welcome to Math Game             #");
+        Console.WriteLine("#           1. New game                      #");
+        Console.WriteLine("#           2. Load game                     #");
+        Console.WriteLine("#           3. View highscores               #");
+        Console.WriteLine("#           4. Quit                          #");
+        Console.WriteLine("##############################################");
+    } while (keyInfo.Key != ConsoleKey.Enter);
+
+    //var menuSelection = Console.ReadLine();
+
+
+}
+
+
+void CreateUser()
+{
+    User user = new User();
+    Console.WriteLine("Enter username to track score: ");
+    user.name = Console.ReadLine();
+    GameSelect();
+}
+
+void GameSelect()
 {
     Console.WriteLine("--------------------------------------------");
-    Console.WriteLine($"Hello {username}, today is {date.DayOfWeek}.\n");
+    Console.WriteLine($"Hello {user.name}, today is {date.DayOfWeek}.\n");
     Console.WriteLine(@$"Please press one of the following keys to play a game:
         A - Addition
         S - Subtraction
@@ -42,22 +115,40 @@ static void Menu(string username, DateTime date)
             break;
     }
 }
-static void Addition()
+void Addition()
 {
     Console.WriteLine("Addition game selected...");
+
+    var random = new Random();
+    var firstNum = random.Next(1, 10);
+    var secondNum = random.Next(1, 10);
+    
+    Console.WriteLine($"{firstNum} + {secondNum} = ?");
+    var result = Console.ReadLine();
+
+    while(result != ($"{firstNum + secondNum}"))
+    {
+        Console.WriteLine("Incorrect. Score reset. Try again.");
+        user.score = 0;
+        Console.WriteLine($"{firstNum} + {secondNum} = ?");
+        result = Console.ReadLine();
+    }
+    user.score++;
+    Console.WriteLine($"Correct! Score: {user.score}");
+    GameSelect();
 }
 
-static void Subtraction()
+void Subtraction()
 {
     Console.WriteLine("Subtraction game selected...");
 }
 
-static void Multiplication()
+void Multiplication()
 {
     Console.WriteLine("Multiplication game selected...");
 }
 
-static void Division()
+void Division()
 {
     Console.WriteLine("Division game selected...");
 }
