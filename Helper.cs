ea153b5f59171de82937e90423934ee1eca12b10
Dylan Maziarek <21125921+m-a-z-z-z@users.noreply.github.com>
2024-06-12 14:46:37 +0100

    Highscore menu added, and highscores viewable by category

--- Helper.cs
+++ Helper.cs
@@ -6,11 +6,16 @@ internal static class Helper
 {
     internal static List<Game> games = new List<Game>
     {
-        new Game(GameType.Addition, 10, "12-06-2024", "John Smith"),
-        new Game(GameType.Subtraction, 7, "10-06-2024", "Smohn Jith"),
+        new Game(GameType.Addition, 0, "12-06-2024", "John Smith"),
+        new Game(GameType.Addition, 29, "12-06-2024", "Khabib"),
+        new Game(GameType.Addition, 14, "12-06-2024", "Islam"),
+        new Game(GameType.Addition, 1, "12-06-2024", "B. Green"),
+        new Game(GameType.Subtraction, 29, "10-06-2024", "Jon Jones"),
+        new Game(GameType.Subtraction, 0, "10-06-2024", "Dummy"),
+        new Game(GameType.Subtraction, 3, "10-06-2024", "A. Pereira"),
+        new Game(GameType.Subtraction, 5, "10-06-2024", "S. Strickland"),
         new Game(GameType.Multiplication, 5, "09-06-2024", "Nhoj Thims"),
-        new Game(GameType.Division, 7, "11-06-2024", "Dat Boi"),
-        new Game(GameType.Addition, 12, "12-06-2024", "Khabib")
+        new Game(GameType.Division, 7, "11-06-2024", "Dat Boi")
     };
 
     internal static void SaveGame(Game game)
@@ -22,17 +27,15 @@ internal static class Helper
         games.Add(game);
     }
 
-    internal static void PrintHistory()
+    internal static void PrintHighscores(List<Game> gamesToPrint)
     {
-        var gamesToPrint = games.Where(x => x.Score > 0)
-            .OrderByDescending(x => x.Score)
-            .ThenBy(x => x.Date);
-
         Console.WriteLine($"\t\t\t\t##############################################");
-        Console.WriteLine("\t\t\tDate\tPlayer name\tGame mode\tScore");
-        foreach (var game in games)
+        Console.WriteLine("\t\t\t\tRank\tScore\tPlayer name\tDate");
+        int rank = 1;
+        foreach (var game in gamesToPrint)
         {
-            Console.WriteLine($"\t\t\t{game.Date:dd-MM-yyyy} --- {game.PlayerName} --- {game.GameMode} --- {game.Score}");
+            Console.WriteLine($"\t\t\t\t{rank}\t{game.Score}\t{game.PlayerName}\t{game.Date:dd-MM-yyyy}");
+            rank++;
         }
         Console.WriteLine($"\t\t\t\t##############################################");
 
--- Menu.cs
+++ Menu.cs
@@ -30,13 +30,13 @@ internal class Menu
             Console.WriteLine($"{tabs}##############################################");
             Console.WriteLine($"{tabs}#            Welcome to Math Game            #");
             Console.WriteLine($"{tabs}#                                            #");
-            Console.WriteLine($"{tabs}#{(selectedOption == 1 ? color : "")}                  New game\u001b[0m                  #");
-            Console.WriteLine($"{tabs}#{(selectedOption == 2 ? color : "")}                  Load game\u001b[0m                 #");
-            Console.WriteLine($"{tabs}#{(selectedOption == 3 ? color : "")}               View highscores\u001b[0m              #");
-            Console.WriteLine($"{tabs}#{(selectedOption == 4 ? color : "")}                    Quit\u001b[0m                    #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 1 ? color : "")}                  Play game\u001b[0m                 #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 2 ? color : "")}               View highscores\u001b[0m              #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 3 ? color : "")}                    Quit\u001b[0m                    #");
             Console.WriteLine($"{tabs}#                                            #");
             Console.WriteLine($"{tabs}#   Use arrows keys to navigate menu items.  #");
             Console.WriteLine($"{tabs}#          Use enter key to select.          #");
+            Console.WriteLine($"{tabs}#                                            #");
             Console.WriteLine($"{tabs}##############################################");
 
             key = Console.ReadKey(true);
@@ -62,27 +62,21 @@ internal class Menu
                     else if (selectedOption == 2)
                     {
                         Console.Clear();
-                        Console.WriteLine($"{tabs}Load game selected");
-                        throw new NotImplementedException();
-                    }
-                    else if (selectedOption == 3)
-                    {
-                        Console.Clear();
-                        Helper.PrintHistory();
+                        Menu.HighScoreMenu();
                         Console.WriteLine($"\n\n{tabs}Press enter to return to menu");
                         Console.ReadLine();
                         Console.Clear();
                         MainMenu();
                     }
-                    else if (selectedOption == 4)
+                    else if (selectedOption == 3)
                     {
                         Console.WriteLine($"{tabs}Exiting game...");
                         Environment.Exit(0);
                     }
                     break;
-            }
-        }
-    }
+            } // end switch
+        } // end while
+    }   // end main menu
 
     internal static void GameSelectMenu()
     {
@@ -101,6 +95,7 @@ internal class Menu
             Console.WriteLine($"{tabs}#{(selectedOption == 2 ? color : "")}                Subtraction\u001b[0m                 #");
             Console.WriteLine($"{tabs}#{(selectedOption == 3 ? color : "")}              Multiplication\u001b[0m                #");
             Console.WriteLine($"{tabs}#{(selectedOption == 4 ? color : "")}                 Division\u001b[0m                   #");
+            Console.WriteLine($"{tabs}#                                            #");
             Console.WriteLine($"{tabs}#{(selectedOption == 5 ? color : "")}            Return to main menu\u001b[0m             #");
             Console.WriteLine($"{tabs}#                                            #");
             Console.WriteLine($"{tabs}##############################################");
@@ -157,49 +152,90 @@ internal class Menu
                         MainMenu();
                     }
                     break;
-            }
-        }
+            }   // end of switch
+        }   // end of while loop
+    }   // end of game select menu
 
+    internal static void HighScoreMenu()
+    {
+        selectedOption = 1;
+        isSelected = false;
+        Console.CursorVisible = false;
+        (int left, int top) = Console.GetCursorPosition();
 
-        //Console.Write($"\n{tabs}Enter selection: ");
-        //var gameSelected = Console.ReadLine();
+        while (!isSelected)
+        {
+            Console.SetCursorPosition(left, top);
 
-        //switch (gameSelected.Trim().ToUpper())
-        //{
-        //    case "A":
-        //        Console.Clear();
-        //        Console.WriteLine($"{tabs}######### Addition game selected... #########\n");
-        //        Game additionGame = new Game(GameType.Addition);
-        //        gameEngine.PlayGame(additionGame, '+');
-        //        break;
-        //    case "S":
-        //        Console.Clear();
-        //        Console.WriteLine($"{tabs}######### Subtraction game selected... #########\n");
-        //        Game subtractionGame = new Game(GameType.Subtraction);
-        //        gameEngine.PlayGame(subtractionGame, '-');
-        //        break;
-        //    case "M":
-        //        Console.Clear();
-        //        Console.WriteLine($"{tabs}######### Multiplication game selected... #########\n");
-        //        Game multiplicationGame = new Game(GameType.Multiplication);
-        //        gameEngine.PlayGame(multiplicationGame, '*');
-        //        break;
-        //    case "D":
-        //        Console.Clear();
-        //        Console.WriteLine($"{tabs}######### Division game selected... #########\n");
-        //        Game divisionGame = new Game(GameType.Division);
-        //        gameEngine.PlayGame(divisionGame, '/');
-        //        break;
-        //    case "R":
-        //        Console.Clear();
-        //        MainMenu();
-        //        break;
-        //    default:
-        //        Console.WriteLine("Invalid selection");
-        //        break;
-        //}
+            Console.WriteLine($"{tabs}##############################################");
+            Console.WriteLine($"{tabs}#      Select highscores for a game mode     #");
+            Console.WriteLine($"{tabs}#                                            #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 1 ? color : "")}                 Addition\u001b[0m                   #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 2 ? color : "")}                Subtraction\u001b[0m                 #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 3 ? color : "")}              Multiplication\u001b[0m                #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 4 ? color : "")}                 Division\u001b[0m                   #");
+            Console.WriteLine($"{tabs}#                                            #");
+            Console.WriteLine($"{tabs}#{(selectedOption == 5 ? color : "")}            Return to main menu\u001b[0m             #");
+            Console.WriteLine($"{tabs}#                                            #");
+            Console.WriteLine($"{tabs}##############################################");
 
+            key = Console.ReadKey(true);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.DownArrow:
+                    selectedOption = (selectedOption == 5 ? 1 : selectedOption + 1);
+                    break;
+
+                case ConsoleKey.UpArrow:
+                    selectedOption = (selectedOption == 1 ? 5 : selectedOption - 1);
+                    break;
+
+                case ConsoleKey.Enter:
+                    isSelected = true;
+                    if (selectedOption == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{tabs}######### Viewing Addition highscores... #########\n");
+                        var gamesToPrint = Helper.games.Where(x => x.Score > 0 && x.GameMode == GameType.Addition)
+                            .OrderByDescending(x => x.Score)
+                            .ThenBy(x => x.Date);
+                        Helper.PrintHighscores(gamesToPrint.ToList());
+                    }
+                    else if (selectedOption == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{tabs}######### Viewing Subtraction highscores... #########\n");
+                        var gamesToPrint = Helper.games.Where(x => x.Score > 0 && x.GameMode == GameType.Subtraction)
+                            .OrderByDescending(x => x.Score)
+                            .ThenBy(x => x.Date);
+                        Helper.PrintHighscores(gamesToPrint.ToList());
+                    }
+                    else if (selectedOption == 3)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{tabs}######### Viewing Multiplication highscores... #########\n");
+                        var gamesToPrint = Helper.games.Where(x => x.Score > 0 && x.GameMode == GameType.Multiplication)
+                            .OrderByDescending(x => x.Score)
+                            .ThenBy(x => x.Date);
+                        Helper.PrintHighscores(gamesToPrint.ToList());
+                    }
+                    else if (selectedOption == 4)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{tabs}######### Viewing Division highscores... #########\n");
+                        var gamesToPrint = Helper.games.Where(x => x.Score > 0 && x.GameMode == GameType.Division)
+                            .OrderByDescending(x => x.Score)
+                            .ThenBy(x => x.Date);
+                        Helper.PrintHighscores(gamesToPrint.ToList());
+                    }
+                    else if (selectedOption == 5)
+                    {
+                        Console.Clear();
+                        MainMenu();
+                    }
+                    break;
+            }   // end of switch
+        }   // end of while loop
     }
-
-
 }
